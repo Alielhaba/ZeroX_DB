0cd0b5076e679f6158608ae455598c599821d3c3
Hussein Ghoraba <husseinghoraba.gh@gmail.com>
2022-12-26 00:03:54 +0200

    One Modification in a line in Main script

--- ZeroXDBMS.sh
+++ ZeroXDBMS.sh
@@ -59,7 +59,6 @@ select choice in "Create Database" "List Database" "Drop Database" "Connect Data
 	case $choice in 
 		"Create Database" )
             createdb
-			re_choose_from_main_list_fun
 			#Part 1-1 #--running create Database script--#
 		;;
 		"List Database" )
